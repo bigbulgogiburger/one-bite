d9ce04bf4c0a1ef23fb339376d308f14ca6e4a9f
pyeondohun <dohoon321@gmail.com>
2025-08-10 23:01:30 +0900

    강의 듣는 중

---
+++ section09/src/components/TodoItem.jsx
@@ -0,0 +1,34 @@
+import './TodoItem.css'
+import { memo } from 'react';   
+const TodoItem = ({id, isDone, content, date,onUpdate,onDelete}) => {
+
+    const onChangeCheckBox = ()=>{
+        onUpdate(id);
+    }
+
+    const onClickDeleteButton = () =>{
+        onDelete(id);
+    }
+
+    return (
+        <div className="TodoItem">
+            <input onChange={onChangeCheckBox} checked={isDone} type="checkbox" />
+            <div className='content'>{content}</div>
+            <div className='date'>{new Date(date).toLocaleDateString()}</div>
+            <button onClick={onClickDeleteButton}>삭제</button>
+        </div>
+    );
+}
+
+export default memo(TodoItem);
+// // 고차 컴포넌트(HOC)
+// export default memo(TodoItem,(prev,next)=>{
+//     //반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+//     // T -> 안바뀜(리렌더링 안함)
+//     // F -> 바뀜(리렌더링 함)
+//     if(prev.id !== next.id) return false;
+//     if(prev.isDone !== next.isDone) return false;
+//     if(prev.content !== next.content) return false;
+//     if(prev.date !== next.date) return false;
+//     return true;
+// });
